2e5d6ff1711664b0d2f3a66887a7cc1a47d9e1a4
Evan Johnson <evan@cloudflare.com>
2018-12-24 10:47:27 -0800

    Reorder functions

--- sshcert.go
+++ sshcert.go
@@ -22,9 +22,6 @@ const (
 
 var (
 	DefaultPermissions = ssh.Permissions{
-		// CriticalOptions: map[string]string{
-		// 	"force-command": "/bin/sh",
-		// },
 		Extensions: map[string]string{
 			"permit-pty":              "",
 			"permit-user-rc":          "",
@@ -44,6 +41,16 @@ type SigningArguments struct {
 	Principals  []string
 	Permissions ssh.Permissions
 	Duration    time.Duration
+}
+
+func NewCA() (SshCa, error) {
+	signer, err := createSSHSigner()
+	if err != nil {
+		return SshCa{}, err
+	}
+	return SshCa{
+		Signer: signer,
+	}, nil
 }
 
 func (s *SshCa) SignCert(pub ssh.PublicKey, signArgs *SigningArguments) (*SshCert, error) {
@@ -68,65 +75,8 @@ func (c *SshCert) String() string {
 	return fmt.Sprintf("%s %s", ssh.CertAlgoECDSA256v01, base64.StdEncoding.EncodeToString(c.Certificate.Marshal()))
 }
 
-func NewCA() (SshCa, error) {
-	signer, err := createSSHSigner()
-	if err != nil {
-		return SshCa{}, err
-	}
-	return SshCa{
-		Signer: signer,
-	}, nil
-}
-
-func ParsePublicKey(pub string) (ssh.PublicKey, error) {
-	pubParts := strings.Split(pub, " ")
-	if len(pubParts) != 2 && len(pubParts) != 3 {
-		return nil, errors.New("Invalid public key format")
-	}
-	pubBytes, err := base64.StdEncoding.DecodeString(pubParts[1])
-	pubKey, err := ssh.ParsePublicKey(pubBytes)
-	if err != nil {
-		return nil, err
-	}
-	return pubKey, nil
-}
-
-// createPrivateKey will create a new ecdsa PrivateKey.
-func createPrivateKey() (*ecdsa.PrivateKey, error) {
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		return &ecdsa.PrivateKey{}, err
-	}
-	return privateKey, nil
-}
-
-func createSSHSigner() (ssh.Signer, error) {
-	key, err := createPrivateKey()
-	if err != nil {
-		return nil, err
-	}
-	signer, err := ssh.NewSignerFromKey(key)
-	if err != nil {
-		return nil, err
-	}
-	return signer, nil
-}
-
-// String will print the public key entry that should be added to the server's sshd_config
 func (s *SshCa) String() string {
 	return fmt.Sprintf("%s %s %s", s.Signer.PublicKey().Type(), base64.StdEncoding.EncodeToString(s.Signer.PublicKey().Marshal()), CAName)
-}
-
-func randomSerial() uint64 {
-	buf := make([]byte, 8)
-	rand.Read(buf)
-	return binary.LittleEndian.Uint64(buf)
-}
-
-func randomHex() string {
-	buf := make([]byte, 16)
-	rand.Read(buf)
-	return hex.EncodeToString(buf)
 }
 
 func NewSigningArguments(principals []string) *SigningArguments {
@@ -147,4 +97,49 @@ func (s *SigningArguments) SetDuration(d time.Duration) {
 
 func (s *SigningArguments) SetPrincipals(principals []string) {
 	s.Principals = principals
+}
+
+func ParsePublicKey(pub string) (ssh.PublicKey, error) {
+	pubParts := strings.Split(pub, " ")
+	if len(pubParts) != 2 && len(pubParts) != 3 {
+		return nil, errors.New("Invalid public key format")
+	}
+	pubBytes, err := base64.StdEncoding.DecodeString(pubParts[1])
+	pubKey, err := ssh.ParsePublicKey(pubBytes)
+	if err != nil {
+		return nil, err
+	}
+	return pubKey, nil
+}
+
+func createPrivateKey() (*ecdsa.PrivateKey, error) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return &ecdsa.PrivateKey{}, err
+	}
+	return privateKey, nil
+}
+
+func createSSHSigner() (ssh.Signer, error) {
+	key, err := createPrivateKey()
+	if err != nil {
+		return nil, err
+	}
+	signer, err := ssh.NewSignerFromKey(key)
+	if err != nil {
+		return nil, err
+	}
+	return signer, nil
+}
+
+func randomSerial() uint64 {
+	buf := make([]byte, 8)
+	rand.Read(buf)
+	return binary.LittleEndian.Uint64(buf)
+}
+
+func randomHex() string {
+	buf := make([]byte, 16)
+	rand.Read(buf)
+	return hex.EncodeToString(buf)
 }
